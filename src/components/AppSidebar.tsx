754c97e0984c973c738c8815836a641efb8ba41b
Siddhant-ui-tech <siddhantd955@gmail.com>
2025-07-04 22:51:31 +0530

    Fix Settings Navigation Label

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -252,7 +252,7 @@ export function AppSidebar() {
                   <SidebarMenuButton asChild>
                     <NavLink to={item.url} className={getNavCls}>
                       <item.icon className="mr-2 h-4 w-4" />
-                      {!isCollapsed && <span>{"Profile"}</span>}
+                      {!isCollapsed && <span>{item.title}</span>}
                     </NavLink>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
